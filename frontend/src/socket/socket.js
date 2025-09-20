ea0c39086be5310c47b8d19cdb5d850dd1b5bf23
Ignacio Gomez <Nachorojo2015@gmail.com>
2025-09-20 19:38:57 -0300

    feat: update socket connection to use environment variable for backend URL

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,2 +1,2 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3000", { withCredentials: true });
+export const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:3000", { withCredentials: true });
